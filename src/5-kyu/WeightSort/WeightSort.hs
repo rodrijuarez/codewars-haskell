22c91196746c338d00400e24f7351bd3e6373c07
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-08-28 20:36:37 +0200

    Simpler version

--- src/5-kyu/WeightSort/WeightSort.hs
+++ src/5-kyu/WeightSort/WeightSort.hs
@@ -1,28 +1,10 @@
 module WeightSort where
 
---import Data.Digits (digits)
-import Data.List (sort)
+import Data.Char
+import Data.List
 
 orderWeight :: [Char] -> [Char]
-orderWeight = unwords . map show . sort . map (FatNumber) . words . take 90
+orderWeight = unwords . sortOn calcWeight . sort . words
 
-digits 0 = []
-digits x = digits (x `div` 10) ++ [x `mod` 10]
-
-data FatNumber =
-  FatNumber (String)
-
-weight :: FatNumber -> Int
-weight (FatNumber x) = sum $ digits $ read x :: Int
-
-instance Show FatNumber where
-  show (FatNumber x) = x
-
-instance Eq FatNumber where
-  (FatNumber x) == (FatNumber y) = x == y
-
-instance Ord FatNumber where
-  compare a b =
-    if (weight a == weight b)
-      then compare (show a) (show b)
-      else compare (weight a) (weight b)
+calcWeight :: String -> Int
+calcWeight = sum . map digitToInt
