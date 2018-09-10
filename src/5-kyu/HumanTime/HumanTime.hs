3e2633f262025da1022d5f0c2658eae95ddc5b46
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-09-10 20:58:22 +0200

    Added best implementation

--- src/5-kyu/HumanTime/HumanTime.hs
+++ src/5-kyu/HumanTime/HumanTime.hs
@@ -11,3 +11,8 @@ showWithTwoDigits :: Int -> String
 showWithTwoDigits n
   | length (show n) == 1 = "0" ++ (show n)
   | otherwise = show n
+-- Best implementation
+--humanReadable :: Int -> String
+--humanReadable x = printf "%02d:%02d:%02d" h m s
+  --where (y, s) = x `divMod` 60
+                --(h, m) = y `divMod` 60
